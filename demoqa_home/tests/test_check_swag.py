5743c1cca7be7f3877723a2cd075423d07baec97
Trevogin <p_trevogin@mail.ru>
2025-11-19 20:42:37 +0300

    Delete demoqa_home directory

--- demoqa_home/tests/test_check_swag.py
+++
@@ -1,9 +0,0 @@
-from pages.swag_labs import SwagLabs
-
-
-def test_check_icon(browser):
-    swag_labs_page = SwagLabs(browser)
-    swag_labs_page.visit()
-    swag_labs_page.exist_icon()
-    swag_labs_page.exist_icon_username()
-    swag_labs_page.exist_icon_password()
