96b2cc592f78c47a3cff7a70bd9e7c8164be895d
Trevogin <p_trevogin@mail.ru>
2025-11-22 22:28:34 +0300

    Add files via upload

---
+++ demoqa/tests/test_seo.py
@@ -0,0 +1,9 @@
+from pages.demoqa import DemoQa
+from pages.elements_page import ElementsPage
+
+
+def test_check_title_demo(browser):
+    demo_qa_page = DemoQa(browser)
+
+    demo_qa_page.visit()
+    assert browser.title == 'DEMOQA'
---
+++ demoqa/tests/tests_hw/test_check_text.py
@@ -0,0 +1,28 @@
+from pages.demoqa import DemoQa
+from pages.elements_page import ElementsPage
+
+
+def test_1(browser):
+    demo_qa_page = DemoQa(browser)
+
+    demo_qa_page.visit()
+    assert demo_qa_page.footer.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+
+
+def test_2(browser):
+    demo_qa_page = DemoQa(browser)
+    elements_page = ElementsPage(browser)
+
+    demo_qa_page.visit()
+    demo_qa_page.btn_elements.click()
+    assert elements_page.text2.get_text() == 'Please select an item from left to start practice.'
+
+
+def test_page_elements(browser):
+    elements_page = ElementsPage(browser)
+    elements_page.visit()
+
+    assert elements_page.icon.exist()
+    assert elements_page.btn_sidebar_first.exist()
+    assert elements_page.btn_sidebar_first_textbox.exist()
+
