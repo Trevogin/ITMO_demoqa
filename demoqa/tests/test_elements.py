b1ffbb74e86b24ea8c7f377baf905def5d512f55
Trevogin <p_trevogin@mail.ru>
2025-11-23 22:10:26 +0300

    Add files via upload

---
+++ demoqa/tests/test_elements.py
@@ -0,0 +1,9 @@
+from itertools import count
+
+from pages.elements_page import ElementsPage
+
+def test_find_elements(browser):
+    elements_page = ElementsPage(browser)
+    elements_page.visit()
+
+    assert elements_page.btns_first_menu.check_count_elements(count=9)
