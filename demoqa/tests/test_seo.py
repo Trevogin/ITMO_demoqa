db5cf569afcf7be3b0b501fb49b535b2d2efbb5e
Trevogin <p_trevogin@mail.ru>
2025-11-24 03:56:36 +0300

    Delete demoqa directory

--- demoqa/tests/test_seo.py
+++
@@ -1,9 +0,0 @@
-from pages.demoqa import DemoQa
-from pages.elements_page import ElementsPage
-
-
-def test_check_title_demo(browser):
-    demo_qa_page = DemoQa(browser)
-
-    demo_qa_page.visit()
-    assert browser.title == 'DEMOQA'
